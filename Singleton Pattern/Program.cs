134572e343a324e860b65ab3b4c224cfa32cdd0e
Md Ali Jaffry <mjaffry04@outlook.com>
2024-10-16 13:15:29 +0530

    Add project files.

---
+++ Singleton Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿public sealed class Singleton
+{
+    private static readonly Singleton _instance = new Singleton();
+
+    // Private constructor prevents instantiation from other classes
+    private Singleton() { }
+
+    public static Singleton Instance => _instance;
+
+    public void DoSomething()
+    {
+        Console.WriteLine("Singleton instance invoked.");
+    }
+}
+
+ class Program
+    {
+    static void Main()
+{
+    Singleton.Instance.DoSomething();
+}
+}
